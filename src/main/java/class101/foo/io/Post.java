994141f5b54b20d33fa1d767ccab784694f0fdff
buchu <qkqdlek32@gmail.com>
2023-07-18 10:21:38 +0900

    es 수정

--- src/main/java/class101/foo/io/Post.java
+++ src/main/java/class101/foo/io/Post.java
@@ -1,9 +1,8 @@
 package class101.foo.io;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
-
-import javax.persistence.Id;
 
 @Data
 @Document(indexName = "post")
